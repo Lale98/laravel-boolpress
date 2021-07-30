e98de14d5e888500afaa76aac171f6bcebffe866
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-07-30 14:03:29 +0200

    Guest posts

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -43,7 +43,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(5);
+        $posts = Post::paginate(6);
 
         return view('admin.posts.index', compact('posts'));
     }
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="text-center mt-4">
+      &copy; Boolean Careers
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+
+</style>
